2cd7a838206363a75a30c3f478d550aa7afaf269
welcome <welcome@Naveen>
2025-06-13 11:12:49 +0530

    my char program

---
+++ C2TC_Core_Java_Program/src/day2/Char.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Char {
+	public static void main(String[] args) {
+		char ch = 'a';
+		System.out.println(ch);
+		char ch1 = 110;
+		System.out.println(ch1);
+		char var1 = '\u00A7';
+		System.out.println(var1);
+		char var2 = '\u20AC';
+		System.out.println(var2);
+		int a = 'n';
+		System.out.println(a);
+	}
+}
