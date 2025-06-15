333b7595c6f000cd8bc81a46b6da3f7b8d3a8247
welcome <welcome@Naveen>
2025-06-15 07:25:26 +0530

    It contains of datatypes, conditional statements & switch cases

---
+++ C2TC_Core_Java_Program/src/day2/Identifiers.java
@@ -0,0 +1,12 @@
+//Program to demonstrate identifiers 
+package day2;
+public class Identifiers {
+	public static void main(String[] args) {
+		int $number = 21; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Naveed"; 
+		System.out.println("value is : "+ studentName);
+	}
+
+}
---
+++ C2TC_Core_Java_Program/src/day3/Continuedemo.java
@@ -0,0 +1,13 @@
+package day3;
+public class Continuedemo {
+	public static void main(String[] args) {
+		for (int k = 5; k < 15; k++)
+		{
+		// Odd numbers are skipped
+		if (k%2 != 0)
+		continue;
+		// Even numbers are printed
+		System.out.print(k + " ");
+		}
+	}
+}
